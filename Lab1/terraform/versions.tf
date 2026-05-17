f98ef5d16534cdcda8f42858a0c7875237bc443f
harrybrwnie <huanphan2497@gmail.com>
2026-05-17 07:00:05 +0000

    Update S3 backend config and DynamoDB

--- Lab1/terraform/versions.tf
+++ Lab1/terraform/versions.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "nt548-terraform-state-bucket-23520552" # Change this to your unique bucket name
+    key            = "lab1/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "nt548-terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
